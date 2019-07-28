c5ca400f47b1e91ef78d6b32428c195777c87104
RCDMK <rcdmk@hotmail.com>
2019-07-27 22:08:51 -0300

    Change app bootstrap code to use router and controller modules

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const controllers = require('./controllers');
+const router = require('./router');
+
+const app = express();
+
+router(app).register(controllers);
+
+const server = app.listen(3000, () => {
+  const serverAddress = server.address();
+
+  console.log(`Server listening at http://${serverAddress.address}:${serverAddress.port}`);
+});
