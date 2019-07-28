34d1000be9a204c1b4efee98390f58e6a018b8c1
RCDMK <rcdmk@hotmail.com>
2019-07-27 22:08:19 -0300

    Add base router module

---
+++ router/index.js
@@ -0,0 +1,16 @@
+/**
+ * Router registers controllers for each route
+ * @param  {object} app Express app
+ * @returns {object} register An object that holds the register method to register routes
+ */
+module.exports = function Router(app) {
+  return {
+    /**
+     * Registers application routes to controller handlers
+     * @param  {object} controllers The controllers module instance with handler properties
+     */
+    register: function registerRoutes(controllers) {
+      app.get('/', controllers.Index);
+    }
+  };
+};
