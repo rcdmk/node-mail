aaa35e86a80e75197b643f7a76978ac343c80e56
RCDMK <rcdmk@hotmail.com>
2019-07-28 20:17:14 -0300

    Remove jshint and install eslint

--- services/email.js
+++ services/email.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// eslint-disable-next-line max-len
 const validEmailRegExp = /^(?:[^<>()\[\]\\.,;:\s@"]+(?:\.[^<>()\[\]\\.,;:\s@"]+)*)@(?:(?:\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(?:(?:[a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 function validateEmailAddress(email) {
