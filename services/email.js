578f4e0adaaec5d1118d0e752f1ea762aad8bfdc
RCDMK <rcdmk@hotmail.com>
2019-07-29 22:01:41 -0300

    Change validation errors to use domain errors

--- services/email.js
+++ services/email.js
@@ -1,4 +1,5 @@
 'use strict';
+const ValidationError = require('../infra/errors').ValidationError;
 
 const validEmailRegExp = /^[a-zA-Z0-9][a-zA-Z0-9._-]*?[a-zA-Z0-9]@[a-zA-Z0-9][a-zA-Z0-9.-]*?\.[a-zA-Z]{2,}$/;
 
@@ -11,20 +12,28 @@ module.exports = function EmailService(options) {
 
   function validateAndFormatSendParams(params) {
     return new Promise((resolve, reject) => {
-      if (!params) return reject(new Error('Params object missing'));
+      if (!params) return reject(new ValidationError('', 'Params object missing'));
 
       if (!Array.isArray(params.to)) params.to = [params.to];
 
       if (params.cc && !Array.isArray(params.cc)) params.cc = [params.cc];
       if (params.bcc && !Array.isArray(params.bcc)) params.bcc = [params.bcc];
 
-      if (!params.to.every(validateEmailAddress)) return reject(new Error('To must be a valid e-mail address'));
-      if (!validateEmailAddress(params.from)) return reject(new Error('From must be a valid e-mail address'));
+      if (!params.to.every(validateEmailAddress)) return reject(new ValidationError('to', 'To must be a valid e-mail address'));
 
-      if (params.cc && !params.cc.every(validateEmailAddress)) return reject(new Error('CC must be a valid e-mail address'));
-      if (params.bcc && !params.bcc.every(validateEmailAddress)) return reject(new Error('BCC must be a valid e-mail address'));
+      if (!validateEmailAddress(params.from)) return reject(new ValidationError('from', 'From must be a valid e-mail address'));
 
-      if (typeof params.subject !== 'string') return reject(new Error('Subject must be present, even if empty'));
+      if (params.cc && !params.cc.every(validateEmailAddress)) {
+        return reject(new ValidationError('cc', 'CC must be a valid e-mail address'));
+      }
+
+      if (params.bcc && !params.bcc.every(validateEmailAddress)) {
+        return reject(new ValidationError('bcc', 'BCC must be a valid e-mail address'));
+      }
+
+      if (typeof params.subject !== 'string') {
+        return reject(new ValidationError('subject', 'Subject must be present, even if empty'));
+      }
 
       resolve(params);
     });
--- services/email.test.js
+++ services/email.test.js
@@ -5,6 +5,7 @@ const chaiAsPromised = require('chai-as-promised');
 
 const emailServiceModule = require('./email');
 const emailProvider = require('./providers').email.test;
+const ValidationError = require('../infra/errors').ValidationError;
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;
@@ -28,7 +29,7 @@ describe('E-mail service', function() {
     });
 
     it('Should return an error if parameters are not specified', function(done) {
-      expect(emailService.send()).to.eventually.be.rejected.and.notify(done);
+      expect(emailService.send()).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
     });
 
     it('Should NOT return an error if all required parameters are specified and valid', function(done) {
@@ -39,19 +40,19 @@ describe('E-mail service', function() {
       it('Should return an error if no sender address is present', function(done) {
         delete messageOptions.from;
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
 
       it('Should return an error if sender address is not a string or array of strings', function(done) {
         messageOptions.from = {'email':'test@domain.com'};
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
 
       it('Should return an error if sender address is not a valid e-mail address', function(done) {
         messageOptions.from = 'someone-at-domain.com';
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
     });
 
@@ -59,25 +60,25 @@ describe('E-mail service', function() {
       it('Should return an error if no primary destination is present', function(done) {
         delete messageOptions.to;
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
 
       it('Should return an error if primary destination is not a valid e-mail address', function(done) {
         messageOptions.to = 'someone-at-domain.com';
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
 
       it('Should return an error if primary destination address is not a string or array of strings', function(done) {
         messageOptions.to = {'email':'test@domain.com'};
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
 
       it('Should return an error if primary destination is an array that contains invalid e-mail addresses', function(done) {
         messageOptions.to = ['address1@domain.com', 'address2-at-domain.com'];
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
 
       it('Should NOT return an error if primary destination is an array of valid e-mail addresses', function(done) {
@@ -91,13 +92,13 @@ describe('E-mail service', function() {
       it('Should return an error if CC destination is an invalid e-mail addresses', function(done) {
         messageOptions.cc = 'address1-at-domain.com';
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
 
       it('Should return an error if CC destination address is not a string or array of strings', function(done) {
         messageOptions.cc = {'email':'test@domain.com'};
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
 
       it('Should NOT return an error if CC destination is a valid e-mail addresses', function(done) {
@@ -109,7 +110,7 @@ describe('E-mail service', function() {
       it('Should return an error if CC destination is an array that contains invalid e-mail addresses', function(done) {
         messageOptions.cc = ['address1@domain.com', 'address2-at-domain.com'];
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
 
       it('Should NOT return an error if CC destination is an array of valid e-mail addresses', function(done) {
@@ -123,13 +124,13 @@ describe('E-mail service', function() {
       it('Should return an error if BCC destination is an invalid e-mail addresses', function(done) {
         messageOptions.bcc = 'address1-at-domain.com';
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
 
       it('Should return an error if BCC destination address is not a string or array of strings', function(done) {
         messageOptions.bcc = {'email':'test@domain.com'};
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
 
       it('Should NOT return an error if BCC destination is a valid e-mail addresses', function(done) {
@@ -141,7 +142,7 @@ describe('E-mail service', function() {
       it('Should return an error if BCC destination is an array that contains invalid e-mail addresses', function(done) {
         messageOptions.bcc = ['address1@domain.com', 'address2-at-domain.com'];
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
 
       it('Should NOT return an error if BCC destination is an array of valid e-mail addresses', function(done) {
@@ -155,13 +156,13 @@ describe('E-mail service', function() {
       it('Should return an error if subject is not present', function(done) {
         delete messageOptions.subject;
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
 
       it('Should return an error if subject is not a string', function(done) {
         messageOptions.subject = ['Some subject'];
 
-        expect(emailService.send(messageOptions)).to.eventually.be.rejected.and.notify(done);
+        expect(emailService.send(messageOptions)).to.eventually.be.rejectedWith(ValidationError).and.notify(done);
       });
 
       it('Should NOT return an error if subject is empty', function(done) {
