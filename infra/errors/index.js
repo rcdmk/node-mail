1b96a996bc1865f5473b34069074f813ce66e9e1
RCDMK <rcdmk@hotmail.com>
2019-07-29 22:00:52 -0300

    Add domain error types

---
+++ infra/errors/index.js
@@ -0,0 +1,44 @@
+class DomainError extends Error {
+  constructor(message) {
+    super(message);
+    // Set error name to class name
+    this.name = this.constructor.name;
+    // remove constructor from stack trace
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+class ResourceNotFoundError extends DomainError {
+  constructor(resource, query) {
+    super(`Resource ${resource} was not found.`);
+    this.data = {
+      resource,
+      query
+    };
+  }
+}
+
+class InternalError extends DomainError {
+  constructor(error) {
+    super(error.message);
+    this.data = {
+      error
+    };
+  }
+}
+
+class ValidationError extends DomainError {
+  constructor(field, message) {
+    super(`${field}: ${message}`);
+    this.data = {
+      field,
+      message
+    };
+  }
+}
+
+module.exports = {
+  InternalError,
+  ResourceNotFoundError,
+  ValidationError
+};
