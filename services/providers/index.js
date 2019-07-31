f9d8839fbf588419b67245e1617ee42ee1a340fb
RCDMK <rcdmk@hotmail.com>
2019-07-30 22:39:22 -0300

    Add SendGrid email provider implementation

--- services/providers/index.js
+++ services/providers/index.js
@@ -1,9 +1,11 @@
 const emailMailgun = require('./emailMailgun');
+const emailSendGrid = require('./emailSendGrid');
 const emailTest = require('./emailTest');
 
 module.exports = {
   email: {
     Mailgun: emailMailgun,
+    SendGrid: emailSendGrid,
     Test: emailTest,
   }
 };
