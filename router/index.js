97c034aa30af6b0e653e7340f15dcafb77474b2d
RCDMK <rcdmk@hotmail.com>
2019-07-30 22:52:59 -0300

    Add error handling to router

--- router/index.js
+++ router/index.js
@@ -16,6 +16,28 @@ class Router {
   register(controllers) {
     this.app.get('/', controllers.index);
     this.app.post('/messages', controllers.sendMessage);
+
+    // Not found
+    this.app.use((req, res, next) => {
+      res.status(404)
+        .send({
+          message: `Route '${req.method} ${req.url}' not found.`
+        });
+
+      return next();
+    });
+
+    // Error handler
+    this.app.use((err, req, res, next) => {
+      res.status(500)
+        .send({
+          name: err.name,
+          message: err.message,
+          stack: process.env['NODE_ENV'] !== 'production' ? err.stack : undefined
+        });
+
+      return next();
+    });
   }
 }
 
