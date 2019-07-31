9e3e3dfbcae88fed7c5a1ba33a7b71927c17cda0
RCDMK <rcdmk@hotmail.com>
2019-07-31 00:16:23 -0300

    Update app to use config module

--- index.js
+++ index.js
@@ -5,49 +5,40 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-
 const request = require('request-promise-native');
 
+const config = require('./config');
 const EmailService = require('./services/email');
 const {Mailgun, SendGrid} = require('./services/providers').email;
-
-const mailgunOptions = {
-  apiKey: '***REMOVED***',
-  domain: 'sandbox220841877a8c45dda263ff7e5abd1f6c.mailgun.org',
-  sender: 'postmaster@sandbox220841877a8c45dda263ff7e5abd1f6c.mailgun.org'
-};
-
-const sendGridOptions = {
-  apiKey: '***REMOVED***',
-  sender: 'test@sendgrid.com'
-};
-
-const emailOptions = {
-  providers: [
-    new Mailgun(request, mailgunOptions),
-    new SendGrid(request, sendGridOptions),
-  ]
-};
-
-const emailService = new EmailService(emailOptions);
-
-
 const APIController = require('./controllers');
 const Router = require('./router');
 
+
+// server bootstrap
 const app = express();
 
 // security middleware
 app.use(cors());
 app.use(helmet());
 
+// utility middleware
 app.use(morgan('common'));
 app.use(bodyParser.json());
 
+// routes
+const emailOptions = {
+  providers: [
+    new Mailgun(request, config.providers.email.mailgun),
+    new SendGrid(request, config.providers.email.sendgrid),
+  ]
+};
+
+const emailService = new EmailService(emailOptions);
 const controllers = new APIController(emailService);
+
 new Router(app).register(controllers);
 
-const server = app.listen(3000, () => {
+const server = app.listen(config.server.port, () => {
   const serverAddress = server.address();
 
   console.log(`Server listening at http://${serverAddress.address}:${serverAddress.port}`);
