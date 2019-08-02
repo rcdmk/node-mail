a2f1e43033788414719273e01433bcdf1191c373
RCDMK <rcdmk@hotmail.com>
2019-08-02 20:41:19 +0000

    Add config overrides through env vars

--- config/index.js
+++ config/index.js
@@ -1,3 +1,13 @@
 const config = require('./config.json');
 
+// Environment variables override
+const serverPort = process.env['PORT'];
+
+const mailgunApiKey = process.env['MAILGUN_API_KEY'];
+const sendGridApiKey = process.env['SENDGRID_API_KEY'];
+
+if (serverPort) config.sever.port = parseInt(serverPort, 10);
+if (mailgunApiKey) config.providers.mailgun.apiKey = mailgunApiKey;
+if (sendGridApiKey) config.providers.sendgrid.apiKey = mailgunApiKey;
+
 module.exports = config;
