cb77383b60b4db807761ed726cc1132b3a27265f
RCDMK <rcdmk@hotmail.com>
2019-08-02 20:17:40 -0300

    Fix error in server port config override

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ const serverPort = process.env['PORT'];
 const mailgunApiKey = process.env['MAILGUN_API_KEY'];
 const sendGridApiKey = process.env['SENDGRID_API_KEY'];
 
-if (serverPort) config.sever.port = parseInt(serverPort, 10);
+if (serverPort) config.server.port = parseInt(serverPort, 10);
 if (mailgunApiKey) config.providers.mailgun.apiKey = mailgunApiKey;
 if (sendGridApiKey) config.providers.sendgrid.apiKey = mailgunApiKey;
 
